a33de659b037ed86f02260f64c2f31d0816822c3
dmk1111 <dima.golysh@gmail.com>
2018-05-06 23:53:00 +0300

    chore: homework 02
    - create 2 modules - DirWatcher & Importer
    - modules are separate
    - process not fixed amount of files
    - add test .csv file

---
+++ modules/importer/Importer.js
@@ -0,0 +1,48 @@
+const CSV = require("csvtojson");
+import { EventEmitter } from "events";
+
+// TODO: parse only CSV files, update _parseCSV logic, fix subtasks
+
+export class Importer extends EventEmitter {
+    constructor(emitter) {
+        super();
+        this.emitter = emitter;
+    }
+
+    import(path) {
+        return new Promise((resolve, reject) => {
+            this._parseCSV(path)
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+    }
+
+    importSync(path, callback) {
+        this._parseCSV(path)
+            .then(data => callback(null, data))
+            .catch(error => callback(error));
+    }
+
+    autoImport() {
+        if (this.emitter !== undefined) {
+            this.emitter.on("dirwatcher:changed", (data) => this.emit("importer:autoimport", data));
+        }
+    }
+
+    _parseCSV(path) {
+        return new Promise((resolve, reject) => {
+            let arr = [];
+            CSV()
+                .fromFile(path)
+                .on('json', (jsonObj) => {
+                    arr.push(jsonObj);
+                })
+                .on('done', (error) => {
+                    if (error) {
+                        return reject(error);
+                    }
+                    resolve(arr);
+                })
+        })
+    }
+}
